a2647a7973a3c6889a8e054a17c2426d342f3c66
Xaxoxuxu <tonkatapk1@gmail.com>
2020-08-16 00:53:51 +0300

    Added !affixes command

---
+++ commands/affixes.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var https = require('https');
+
+module.exports = {
+    name: 'affixes',
+    cooldown: 20,
+    description: 'Display this week\'s mythic+ affixes (EU)',
+    aliases: ['affix', 'm+', 'mythic'],
+    guildOnly: false,
+    execute(message, args) {
+        const options = {
+            host: "raider.io",
+            port: 443,
+            path: '/api/v1/mythic-plus/affixes?region=eu&locale=en',
+            method: 'GET'
+        };
+
+        https.request(options, function (res) {
+            //console.log('STATUS: ' + res.statusCode);
+            res.setEncoding('utf8');
+
+            let allData = '';
+            res.on('data', function (chunk) {
+                allData += chunk;
+            });
+
+            res.on('end', function () {
+                const parsedData = JSON.parse(allData);
+                let finalMsg = `**Affixes:** ${parsedData.title}\n`;
+                // affix details
+                for (let i = 0; i < 4; i++) {
+                    finalMsg += `**${parsedData.affix_details[i].name}**: ${parsedData.affix_details[i].description}\n`;
+                }
+                finalMsg += `**Leaderboards:** ${parsedData.leaderboard_url}\n`;
+
+                message.channel.send(finalMsg);
+            });
+        }).end();
+    },
+};
